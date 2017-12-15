21c2733d5325b41ab4d881cbff56e48aead76439
Andre Cortes <andre.crts@gmail.com>
2017-12-15 16:15:38 -0600

    it works but needs refactor'

--- src/MessengerPlugin.jsx
+++ src/MessengerPlugin.jsx
@@ -1,34 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import RandomString from 'random-string';
 
 export default class MessengerPlugin extends Component {
     static propTypes = {
         appId: PropTypes.string.isRequired,
         pageId: PropTypes.string.isRequired,
         FB: PropTypes.object,
-        passthroughParams: (props, propName) => {
-            if (propName in props) {
-                const value = props[propName];
-
-                if (typeof value !== 'string') {
-                    return new Error(`Invalid prop ${propName}: must be a string.`);
-                }
-
-                if (props[propName].length > 250) {
-                    return new Error(`Invalid prop ${propName}: must be smaller than 250 characters.`);
-                }
-            }
-        },
-        type: PropTypes.oneOf(['send-to', 'message-us']),
+        user_ref:PropTypes.string,
+        type: PropTypes.oneOf(['send-to', 'message-us','fb-messenger-checkbox']),
         color: PropTypes.oneOf(['blue', 'white']),
+        prechecked: PropTypes.oneOf(['true','false']),
+        allow_login: PropTypes.oneOf(['true','false']),
         size: PropTypes.oneOf(['standard', 'large', 'xlarge'])
     };
+
+
 
     static defaultProps = {
         color: 'blue',
         size: 'standard',
-        type: 'send-to'
+        prechecked:'true',
+        allow_login:'true',
+        user_ref:'yalo_'+RandomString({length:20}),
+        type: 'fb-messenger-checkbox'
     }
 
     initFacebookSDK() {
@@ -44,6 +39,7 @@ export default class MessengerPlugin extends Component {
     }
 
     componentDidMount() {
+        this.props.onRef(this);
         this.initFacebookSDK();
     }
 
@@ -51,11 +47,21 @@ export default class MessengerPlugin extends Component {
         this.initFacebookSDK();
     }
 
+    componentWillUnmount() {
+        this.props.onRef(undefined)
+      }
+
+    send(ref) {
+        const {appId, pageId, user_ref} = this.props;
+        FB.AppEvents.logEvent('MessengerCheckboxUserConfirmation', null, { app_id:appId, page_id:pageId, ref, user_ref });
+    }
+
     render() {
-        const {type, appId, passthroughParams, color, size, pageId} = this.props;
+        const {type, appId, color, size, pageId,prechecked,allow_login,user_ref} = this.props;
         // use dangerouslySetInnerHTML because React will ignore custom attributes
+        const origin = window.location.href;
         const markup = {
-            __html: `<div class=${type === 'send-to' ? 'fb-send-to-messenger' : 'fb-messengermessageus'} messenger_app_id=${appId} page_id=${pageId} data-ref=${passthroughParams} color=${color} size=${size}></div>`
+            __html: '<div class=' + type +' origin='+origin+ ' messenger_app_id=' + appId + ' page_id=' + pageId + ' user_ref=' + user_ref  + ' size=' + size + ' prechecked='+ prechecked + ' allow_login='+allow_login+ '></div>'
         };
         return <div dangerouslySetInnerHTML={markup}></div>;
     }
