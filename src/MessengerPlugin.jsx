8c55f3da351bbe3742058cb44b4524f7b7010964
Andre Cortes <andre.crts@gmail.com>
2017-12-16 01:43:25 -0600

    Remove unused props

--- src/MessengerPlugin.jsx
+++ src/MessengerPlugin.jsx
@@ -8,20 +8,16 @@ export default class MessengerPlugin extends Component {
         pageId: PropTypes.string.isRequired,
         FB: PropTypes.object,
         user_ref:PropTypes.string,
-        type: PropTypes.oneOf(['send-to', 'message-us','fb-messenger-checkbox']),
-        color: PropTypes.oneOf(['blue', 'white']),
         prechecked: PropTypes.oneOf(['true','false']),
         allow_login: PropTypes.oneOf(['true','false']),
         size: PropTypes.oneOf(['standard', 'large', 'xlarge'])
     };
 
     static defaultProps = {
-        color: 'blue',
         size: 'standard',
         prechecked:'true',
         allow_login:'true',
         user_ref:'yalo_'+RandomString({length:20}),
-        type: 'fb-messenger-checkbox'
     }
 
     initFacebookSDK() {
@@ -61,7 +57,7 @@ export default class MessengerPlugin extends Component {
         // use dangerouslySetInnerHTML because React will ignore custom attributes
         const origin = window.location.href;
         const markup = {
-            __html: '<div class=' + type +' origin='+origin+ ' messenger_app_id=' + appId + ' page_id=' + pageId + ' user_ref=' + user_ref  + ' size=' + size + ' prechecked='+ prechecked + ' allow_login='+allow_login+ '></div>'
+            __html: '<div class=fb-messenger-checkbox' +' origin='+origin+ ' messenger_app_id=' + appId + ' page_id=' + pageId + ' user_ref=' + user_ref  + ' size=' + size + ' prechecked='+ prechecked + ' allow_login='+allow_login+ '></div>'
         };
         return <div dangerouslySetInnerHTML={markup}></div>;
     }
